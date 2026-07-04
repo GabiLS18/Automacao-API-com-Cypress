6b5038650ff930ca83fe38c65c5d98503d5645e8
Gabrielle <gabi_luz25@hotmail.com>
2026-07-04 13:57:28 -0300

    segundo commit

--- cypress/e2e/get.api.cy.js
+++ cypress/e2e/get.api.cy.js
@@ -1,10 +1,43 @@
 /// <reference types="cypress"/>
 
-describe ('Buscar dispositivos', () => {
+describe('Buscar dispositivos', () => {
 
 
-  it('', () => {
+  it('Buscar um dispositivo específico', () => {
+
+    const device_id = '7'
+
+    cy.request({
+      method: 'GET',
+      url: `https://api.restful-api.dev/objects/${device_id}`,
+      failOnStatusCode: false
+    }).as('getDeviceResult')
+
+    //validações
+    cy.get('@getDeviceResult')
+      .then((response) => {
+        expect(response.status).equal(200)
+
+        expect(response.body).not.empty //serve para verificar que não está vazio o campo
+
+        expect(response.body.id).equal(device_id)
+        expect(response.body.name).equal('Apple MacBook Pro 16')
+
+        expect(response.body.data).not.empty
+
+        expect(response.body.data.year).not.string // quando é numerico. 
+        expect(response.body.data.year).equal(2019)
+
+        expect(response.body.data.price).not.string
+        expect(response.body.data.price).equal(1849.99)
+
+        expect(response.body.data['CPU model']).not.empty
+        expect(response.body.data['CPU model']).equal('Intel Core i9')
+
+        expect(response.body.data['Hard disk size']).not.empty
+        expect(response.body.data['Hard disk size']).equal('1 TB')
 
 
+      })
   })
 })
---
+++ cypress/e2e/post.api.cy.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress"/>
+
+describe('Cadastro de dispositivos', () => {
+
+
+    it('Cadastrar um dispositivo', () => {
+
+        const body = {
+            "name": "Celular Teste",
+            "data": {
+                "year": 2025,
+                "price": 1849.99,
+                "CPU model": "Intel Core i9",
+                "Hard disk size": "500 GB",
+                "owner": "Qazando LTDA"
+            }
+        }
+
+        cy.request({
+            method: 'POST',
+            url: 'https://api.restful-api.dev/objects/',
+            failOnStatusCode: false,
+            body: body
+        }).as('postDeviceResult')
+
+        //validações
+        cy.get('@postDeviceResult')
+        .then((response) => {
+            expect(response.status).equal(200)
+            expect(response.body.id).not.empty
+            expect(response.body.createdAt).not.string
+
+
+        })
+    })
+})
