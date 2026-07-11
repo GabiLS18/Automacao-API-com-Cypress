5404e862e1fbba5c7f635c27593f9f33bd1e61c9
Gabrielle <gabi_luz25@hotmail.com>
2026-07-11 15:25:56 -0300

    atualização codigo

--- cypress/e2e/post.api.cy.js
+++ cypress/e2e/post.api.cy.js
@@ -29,7 +29,22 @@ describe('Cadastro de dispositivos', () => {
             expect(response.status).equal(200)
             expect(response.body.id).not.empty
             expect(response.body.createdAt).not.string
+            expect(response.body.name).equal("Celular Teste")
 
+            expect(response.body.data.year).not.string
+            expect(response.body.data.year).equal(2025)
+
+            expect(response.body.data.price).not.string
+            expect(response.body.data.price).equal(1849.99)
+
+            expect(response.body.data['CPU model']).not.empty
+            expect(response.body.data['CPU model']).equal('Intel Core i9')
+
+            expect(response.body.data['Hard disk size']).not.empty
+            expect(response.body.data['Hard disk size']).equal('500 GB')
+
+            expect(response.body.data.owner).not.empty
+            expect(response.body.data.owner).equal('Qazando LTDA')
 
         })
     })
